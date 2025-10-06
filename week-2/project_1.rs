03790700660110d0468b50d0201837599e602643
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-10-07 00:06:06 +0100

    (Class assessment Practice)

--- week-2/project_1.rs
+++ week-2/project_1.rs
@@ -1,3 +1,27 @@
 fn main(){
-	
+
+	//listing out the available parameters
+	let p:f64 = 520_000_000.00;
+	let r:f64 = 10.0;
+	let _n:f64 = 5.0;
+	//representing (r)/100.0 as x
+	let x:f64 = (r)/100.0;
+
+	//Getting the sum of x
+    println!("The rate divided by 100 is {}", x );
+
+	//Finding the value of p * (1.0 + x)
+	let _y = p * (1.0 + x);
+	let q = 1.0 + x;
+	//Getting the fifth root of the function
+	let z = q * q * q * q *  q;
+	//getting the value for the amount
+	let a = p * z;
+	println!("The amount is {}", a);
+
+
+	//compound interest
+	let ci = a - p;
+	println!("The compound interest is {}", ci);
 }
+
---
+++ week-2/project_2.rs
@@ -0,0 +1,27 @@
+fn main(){
+	//Listing all parameters 
+	let a:f64 = 450_000.00;
+	let b:f64 = 1_500_000.00;
+	let c:f64 = 750_000.00;
+	let d:f64 = 2_850_000.00;
+	let e:f64 = 250_000.00;
+
+	//Outputing the represented variables
+	println!("The amount of Toshiba is {}", a );
+	println!("The amount of Mac is {}", b );
+	println!("The amount of Hp is {}", c );
+	println!("The amount of Dell is {}", d );
+	println!("The amount of Acer is {}", e );
+
+
+	//Calculationg the sum 
+
+	let _f = (a * 2.0) + (b * 1.0) + (c * 3.0) + (d * 3.0) + (e * 1.0);
+	println!("The sum is {}", _f);
+
+	//Calculating the average
+
+    let m = (a * 2.0) + (b * 1.0) + (c * 3.0) + (d * 3.0) + (e * 1.0)/10.0;
+    println!("The average is {}", m);
+
+}
