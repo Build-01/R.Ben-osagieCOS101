c26581201064108740790c7214457eb054b0187c
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-10-06 15:01:24 +0100

    (class practice assessments)

--- week-2/practice_1.rs
+++ week-2/practice_1.rs
@@ -5,6 +5,6 @@ fn main(){
 		    2. Develop some techniques in computer science,/n
 		    3. Understand the different areas of study in computer science,/n
 		    4. Conversant with applications of computer science,n/
-		    5. Navigate the career prospects in computer science,/n
+		    5. Navigate the career prospects in computer science./n
 		    6. Conversant with computer programming concepts.");
 }
