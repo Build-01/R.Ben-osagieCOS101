45be571a73e3e6cd7d12a1d053a1c8bc22906298
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-10-06 11:57:42 +0100

    (Class practices)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main(){
+	println!(); //prints just a 
+
+	println!("hello"); //prints
+
+	println!("format {} arguments", "some"); //prints format same arguments
+	
+	println!("My name is Ruth. I am the class rep of stream 1", "Dagogo Willam-Jumbo");
+
+
+}
