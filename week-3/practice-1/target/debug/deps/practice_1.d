ea2b30fcc94ef75c58a2a90e0ab5628377d76830
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-10-13 13:21:51 +0100

    Class practice assessments

---
+++ week-3/practice-1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-3\practice-1\target\debug\deps\practice_1.d: src\main.rs
+
+C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-3\practice-1\target\debug\deps\practice_1.exe: src\main.rs
+
+src\main.rs:
---
+++ week-3/practice-1/target/debug/practice-1.d
@@ -0,0 +1,1 @@
+C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-3\practice-1\target\debug\practice-1.exe: C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-3\practice-1\src\main.rs
