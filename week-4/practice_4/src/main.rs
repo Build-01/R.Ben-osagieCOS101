7dd828e1fdb7f2a54d1b3fa77991adb31947bbcd
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-10-20 11:32:41 +0100

    (My Week-4 Practices)

--- week-4/practice_4/src/main.rs
+++ week-4/practice_4/src/main.rs
@@ -1,3 +1,26 @@
+// Rust program to determine age pass
+
+
+ use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+    
+
+    println!("Enter your name: ");
+    io::stdin().read_line(&mut input1).expect("Not a valid string");
+
+
+
+    println!("Enter your age: ");
+    io::stdin().read_line(&mut input2).expect("Not a valid string");
+    let age:i32 = input2.trim().parse().expect("Not a valid number");
+
+    if base >=  18 {
+        println!("Welcome to the party {}!", input1);
+    } else {
+        println!("Oops, you are not of age to enter into the party{}", input1);
+    }
+
 }
---
+++ week-4/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-4\practice_5\target\debug\practice_5.exe: C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-4\practice_5\src\main.rs
