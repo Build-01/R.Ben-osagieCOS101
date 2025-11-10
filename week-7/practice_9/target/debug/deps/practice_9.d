2c8267c273d8a7c9b9f74654896d85c91574e77e
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-11-10 14:34:43 +0100

    (Array with for loop user iter())

---
+++ week-7/practice_9/target/debug/deps/practice_9.d
@@ -0,0 +1,5 @@
+C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-7\practice_9\target\debug\deps\practice_9.d: src\main.rs
+
+C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-7\practice_9\target\debug\deps\practice_9.exe: src\main.rs
+
+src\main.rs:
