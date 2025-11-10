e5e3d848b72c2adfb534383a5c75db01d6d3c3cd
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-11-10 14:31:11 +0100

    (Array with for loop)

---
+++ week-7/practice_7/target/debug/deps/pracice_7.d
@@ -0,0 +1,5 @@
+C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-7\practice_7\target\debug\deps\pracice_7.d: src\main.rs
+
+C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-7\practice_7\target\debug\deps\pracice_7.exe: src\main.rs
+
+src\main.rs:
---
+++ week-7/practice_7/target/debug/pracice_7.d
@@ -0,0 +1,1 @@
+C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-7\practice_7\target\debug\pracice_7.exe: C:\Users\bensa\Documents\R.Ben-osagieCOS101\week-7\practice_7\src\main.rs
