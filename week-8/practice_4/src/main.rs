40a95ecfd780b22282dd6267552365b3ae98a88d
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-11-17 11:49:45 +0100

    (Iterating on the vector)

---
+++ week-8/practice_4/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+  // Name vector 
+  let name = vec!["Ruth", "Sam", "Chizaram", "Chiemelie", "Mosope", "Kelita", "Mark", "Igie", "Sharon", "Olaitan"]
+
+  // Age vector
+  let age = vec![16,17,18,19,20,22,23,14,32,44];
+
+  print!("\nAge allocation:\n");
+  for i in 0..age.len()
+  {
+    //itenating through i on the vector
+    print!("{} is {} years old\n",name[i], age[i]);
+  }
+}
