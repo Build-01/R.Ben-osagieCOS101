a575aeb6224b1f4ab2185ee58f8442a9ff7a2b41
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-11-29 23:57:36 +0100

    Student info file with rust

--- week-9/project_2/src/main.rs
+++ week-9/project_2/src/main.rs
@@ -1,36 +1,25 @@
 use std::io::Write;
 
 fn main() {
-    let student_name = vec!["- Oluchi Mordi", "- Adams Aliyu", "- Shina Bolade", "- Adekunle Gold", "- Bianca Edemoh"];
-    let matric_number = vec!["- ACC10211111", "- ECO10110101", "- CSC10328828", "- EEE11020202", "- MEE10202001"];
-    let department = vec!["- Accounting", "- Economics", "- Computer", "- Electrical", "- Mechanical"];
-    let level = vec!["- 300", "- 100", "- 200", "- 200", "- 100"];
+    let headers = vec!["Student name", "Matric.Number", "Department", "Level"];
+    let students = vec![
+        vec!["Oluchi Mordi", "ACC10211111", "Accounting", "300"],
+        vec!["Adams Aliyu", " ECO10110101", "Economics", " 100"],
+        vec!["Shania Bolade", "CSC10328828", "Computer", "200"],
+        vec!["Adekunle Gold", "EEE11020202", "Electrical", "200"],
+        vec!["Bianca Edemoh", "MEE10202001", "Mechanical", "100"],
+    ];
 
-    let mut file = std::fs::File::create("data.txt").expect("Create failed");
+    let mut file = std::fs::File::create("PAU_SMIS.txt").expect("Create failed");
     
-    // Write headers
-    file.write_all("Student Name\n".as_bytes()).expect("Write failed");
-    for name in &student_name {
-        file.write_all(name.as_bytes()).expect("Write failed");
-        file.write_all("\n".as_bytes()).expect("Write failed");
-    }
+    // Write headers row
+    let header_line = headers.join("\t");
+    writeln!(file, "{}", header_line).expect("Write failed");
     
-    file.write_all("\nMatric Number\n".as_bytes()).expect("Write failed");
-    for matric in &matric_number {
-        file.write_all(matric.as_bytes()).expect("Write failed");
-        file.write_all("\n".as_bytes()).expect("Write failed");
-    }
-    
-    file.write_all("\nDepartment\n".as_bytes()).expect("Write failed");
-    for dept in &department {
-        file.write_all(dept.as_bytes()).expect("Write failed");
-        file.write_all("\n".as_bytes()).expect("Write failed");
-    }
-    
-    file.write_all("\nLevel\n".as_bytes()).expect("Write failed");
-    for lvl in &level {
-        file.write_all(lvl.as_bytes()).expect("Write failed");
-        file.write_all("\n".as_bytes()).expect("Write failed");
+    // Write each student as a row
+    for student in students {
+        let student_line = student.join("\t");
+        writeln!(file, "{}", student_line).expect("Write failed");
     }
     
     println!("\nStudent data saved successfully!");
