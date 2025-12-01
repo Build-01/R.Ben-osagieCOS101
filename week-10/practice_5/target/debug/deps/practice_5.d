0df7071d67d0e699026994b8b38f9a12d4769957
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-12-01 11:04:49 +0100

    Borrowing in rust

---
+++ week-10/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\bensa\documents\r.ben-osagiecos101\week-10\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+C:\Users\bensa\documents\r.ben-osagiecos101\week-10\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\bensa\documents\r.ben-osagiecos101\week-10\practice_5\target\debug\practice_5.exe: C:\Users\bensa\documents\r.ben-osagiecos101\week-10\practice_5\src\main.rs
