d1e13ca6570e0175e5c0c95238fbdd71697a2a17
build_4 <ruth.ben-osagie@pau.edu.ng>
2025-12-01 11:41:22 +0100

    Working with method in RUST

---
+++ week-10/practice_9/target/debug/deps/practice_10.d
@@ -0,0 +1,5 @@
+C:\Users\bensa\documents\r.ben-osagiecos101\week-10\practice_9\target\debug\deps\practice_10.d: src\main.rs
+
+C:\Users\bensa\documents\r.ben-osagiecos101\week-10\practice_9\target\debug\deps\practice_10.exe: src\main.rs
+
+src\main.rs:
---
+++ week-10/practice_9/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\bensa\documents\r.ben-osagiecos101\week-10\practice_9\target\debug\practice_10.exe: C:\Users\bensa\documents\r.ben-osagiecos101\week-10\practice_9\src\main.rs
